deaca048359f2b63538aa388b96999ba266d7c4e
Zillan-Zalilla-Indrikh <60680604+Zillan-Zalilla-Indrikh@users.noreply.github.com>
2020-04-18 11:36:41 +0700

    Add files via upload

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = 'rgba(0,0,0,0.7)';
+
+export default {
+  tintColor,
+  tabIconDefault: 'rgba(0,0,0,0.7)',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
